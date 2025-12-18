44cdd6b7d63f3e66b8aedf1939841acde3481994
bwe77 <137379557+bwe77@users.noreply.github.com>
2025-12-18 19:48:05 +0700

    starte filter

--- backend/src/main/java/com/project/password/manager/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/project/password/manager/security/JwtAuthenticationFilter.java
@@ -2,9 +2,47 @@ package com.project.password.manager.security;
 
 import org.springframework.stereotype.Component;
 
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Jwt;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+import java.io.IOException;
+
+import jakarta.annotation.Nonnull;
+
+
 @Component
-public class JwtAuthenticationFilter {
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
     // Extract JWT from request
     // Validate token
     // Set authentication in SecurityContext
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+        @Nonnull HttpServletRequest request, 
+        @Nonnull HttpServletResponse response, 
+        @Nonnull FilterChain filterChain) 
+            throws ServletException, IOException {
+
+            final String authHeader = request.getHeader("Authorization");
+            final String jwt;
+            final String userEmail;
+            
+            if(authHeader == null || !authHeader.startsWith("Bearer ")){
+                filterChain.doFilter(request, response);
+                return;
+            }
+
+            jwt = authHeader.substring(7);
+            userEmail = jwtService.extractUsername(jwt);
+
+    }
 }
---
+++ backend/src/main/java/com/project/password/manager/security/JwtService.java
@@ -0,0 +1,13 @@
+package com.project.password.manager.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUsername(String jwt) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
